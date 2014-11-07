1507b9e7c3c5ed40350926ea83e1436c019d2a3d
Jonathan Daggerhart <jonathan@daggerhart.com>
2014-11-07 11:55:31 -0500

    not sure where i'm going with this

--- data.js
+++ data.js
@@ -12,14 +12,22 @@ var pc = {
 
 var encounters = [
   {
-    'name': 'Gray sweatsuit',
-    'description': 'sweaty sweaty',
-    'dc': 2
+    'name': 'Wandering peddler',
+    'description': 'He seems shady, and tries to pick your pocket!',
+    'dc': 2,
+    'vs': 'mind'
   },
   {
-    'name': 'Mostly-functional Heat Rifle',
-    'description': 'somewhat functional',
-    'dc': 2
+    'name': 'Pit trap',
+    'description': 'What a nice day to go for a wa... wtf! oh shit!',
+    'dc': 2,
+    'vs': 'spirit'
+  },
+  {
+    'name': 'Skeleton',
+    'description': 'Neat, it still has some of its flesh.',
+    'dc': 2,
+    'vs': 'body'
   }
 ];
 
--- functions.js
+++ functions.js
@@ -2,7 +2,14 @@ function __d( data ){
   console.log( data );
 }
 
-// show current encounter
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+/*/ show current encounter
 function _current_encounter(){
   // loop through adventure encounters
   for( var i = 0; i < adventure.encounters.length; i++ ){
@@ -10,5 +17,39 @@ function _current_encounter(){
       var this_encounter = adventure.encounters[ i ];
       __d( this_encounter );
     }
+  }
+}
+// */
+
+function _next_encounter(){
+  var next = adventure.current_encounter + 1;
+
+  if ( adventure.encounters[ next ] ){
+    adventure.current_encounter = next;
+  }
+  else {
+    __d('End of adventure');
+  }
+}
+
+//
+function _do_current_encounter(){
+  if ( adventure.encounters[ adventure.current_encounter ] ) {
+    var current_encounter = adventure.encounters[ adventure.current_encounter ];
+
+    if ( pc[ current_encounter.vs ] ){
+      var attack = rand(1, pc[ current_encounter.vs ]);
+
+      __d(attack + ' vs ' + current_encounter.dc );
+
+      if ( attack >= current_encounter.dc ){
+        __d('you won the battle!');
+      }
+      else {
+        __d('you lost the battle');
+      }
+    }
+
+    _next_encounter();
   }
 }
