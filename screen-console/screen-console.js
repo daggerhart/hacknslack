dff0f54f96a0aa43e31b91951b9482a4a7b3ec9f
daggerhart <jonathan@daggerhart.com>
2014-11-06 22:00:15 -0500

    basic console setup

---
+++ screen-console/screen-console.js
@@ -0,0 +1,11 @@
+(function($){
+
+  $(document).ready(function(){ 
+    
+    var $screen_console_form = $('#screen-console-form');
+    
+    $screen_console_form.css("border", "1px solid red");
+    
+  });
+  
+})(jQuery);
