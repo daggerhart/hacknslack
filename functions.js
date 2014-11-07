723e8a321f1a52729deb76ff46da13e9111761b3
Jonathan Daggerhart <jonathan@daggerhart.com>
2014-11-07 11:36:08 -0500

    debug wrapper function __d

--- functions.js
+++ functions.js
@@ -1,10 +1,14 @@
+function __d( data ){
+  console.log( data );
+}
+
 // show current encounter
 function _current_encounter(){
   // loop through adventure encounters
   for( var i = 0; i < adventure.encounters.length; i++ ){
     if ( adventure.current_encounter == i ) {
       var this_encounter = adventure.encounters[ i ];
-      console.log(this_encounter);
+      __d( this_encounter );
     }
   }
 }
