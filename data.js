3a6c02a4e6624d392da8c07586c5ea1d88a601eb
Jonathan Daggerhart <jonathan@daggerhart.com>
2014-11-07 11:30:18 -0500

    cleaning up a little

---
+++ data.js
@@ -0,0 +1,32 @@
+var user = {
+  'name': 'nate'
+};
+
+var pc = {
+  'player': user,
+  'mind': 5,
+  'body': 5,
+  'spirit': 5,
+  'hp': 10
+};
+
+var encounters = [
+  {
+    'name': 'Gray sweatsuit',
+    'description': 'sweaty sweaty',
+    'dc': 2,
+  },
+  {
+    'name': 'Mostly-functional Heat Rifle',
+    'description': 'somewhat functional',
+    'dc': 2,
+  }
+];
+
+var adventure = {
+  'current_encounter': 0,
+  'encounters': encounters
+};
+
+var game = {
+};
---
+++ functions.js
@@ -0,0 +1,11 @@
+// show current encounter
+
+function _current_encounter(){
+  // loop through adventure encounters
+  for( var i = 0; i < adventure.encounters.length; i++ ){
+    if ( adventure.current_encounter == i ) {
+      var this_encounter = adventure.encounters[ i ];
+      console.log(this_encounter);
+    }
+  }
+}
