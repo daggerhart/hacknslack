d196914a92f2f5fd9bc1238ad2cdd6570f70879c
BDaggerhart <brandon.daggerhart@gmail.com>
2014-11-26 14:43:20 -0500

    added some encounters

--- hacknslack.dev/game/static/encounters/monsters.js
+++ hacknslack.dev/game/static/encounters/monsters.js
@@ -32,5 +32,37 @@ module.exports = [
     challenge: {
       attribute: 'mind'
     }
+  },
+  {
+    title: 'Flying Monkeys',
+    desc: '',
+    tags: ['creature'],
+    challenge: {
+      attribute: 'body'
+    }
+  },
+  {
+    title: 'Forlorn Goblin Chef',
+    desc: 'He just can\'t seem to get his pasta to cook properly.',
+    tags: ['monster'],
+    challenge: {
+      attribute: 'body'
+    }
+  },
+  {
+    title: 'Misanthropic Cat Ninja',
+    desc: 'It\'s a cat, it\'s a ninja, and it\'s a sociopath.  The odds are not in your favor.',
+    tags: ['monster'],
+    challenge: {
+      attribute: 'body',
+    }
+  },
+  {
+    title: 'Grand Riddlemeister of Carpathia',
+    desc: 'Viggo\'s Riddlemeister was notorious for his cruelty.  Arrested, hanged, stabbed, drawn, and quartered after calling his master "Viggo the Butch."',
+    tags: ['monster'],
+    challenge: {
+      attribute: 'mind',
+    }
   }
 ]
