42fd319fe2b71937509c2faf64acec05ee82299a
Michael Wright <brainfacemike@gmail.com>
2014-12-06 17:11:45 -0500

    added a few class stubs (enough to have... eh... 4 choices. the four archetypes! there is no heart)

---
+++ hacknslack.dev/game/static/classes/alchemist.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'alchemist',
+	description: 'An ambitious and often avaracious '
+	 + 'adventurer auctioning oddities.',
+	attributes: {
+	  mind: "strong",
+	  hp: "weak",
+	},
+	actions: [
+		{
+			cmd: "brew",
+			text: "pour yourself a brew",
+		}
+	],
+	brew: function( Game, done ) {
+		// TODO: usefulness
+		Game.output("REFRESHING");
+		done();
+	},
+  // TODO: ITEMS!
+}
---
+++ hacknslack.dev/game/static/classes/savage.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'savage',
+	description: 'Rick flair is a semi-retired professional wrestler currently signed with no one.',
+ 
+	attributes: {
+	  body: "strong",
+	  mind: "weak",
+	},
+	actions: [
+		{
+			cmd: "WOOO",
+			text: "FLIP THE FUCK OUT",
+		}
+	],
+	rage: function( Game, done ) {
+		// TODO: usefulness
+		Game.output("CALM DOWN BRAH");
+		done();
+	},
+  // TODO: ITEMS!
+}
