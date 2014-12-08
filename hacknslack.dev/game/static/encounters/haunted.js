8da7d507717d4a64515b157b5b3fece2ace264a7
BDaggerhart <brandon.daggerhart@gmail.com>
2014-12-08 14:01:36 -0500

    more encounters

--- hacknslack.dev/game/static/encounters/haunted.js
+++ hacknslack.dev/game/static/encounters/haunted.js
@@ -26,5 +26,63 @@ module.exports = [
     challenge: {
       attribute: 'body'
     }
+  },
+  {
+    title: 'The King\'s Spirit',
+    desc: 'Standing before you is the ghostly apparition of a helmed man.  The flickering visage beckons you closer. "Do not fear," it says in a gravelly voice.  "I just want to talk."',
+    tags: ['haunted'],
+    challenge: {
+      attribute: 'spirit',
+    },
+    attack_alias: ['donotfear', '"I am not afraid, I am not afraid, I am not afraid," you repeat to yourself, approaching the spirit.'],
+    success: [
+      { type: 'output', amount: 'You overcome your fear.  The ghostly man takes off his helmet.  "I am the long-dead first king of Azkahdo-Tilah-Solfa-Mirehdo.  I was murdered in my sleep by the Guild of Brevity for naming my kingdom such.  Here is my tale...'},
+      { type: 'output', amount: 'Much later, you realize you have spent the last five hours listening to this old coot ramble on about his kingdom.  It appears the king is finally wrapping up.'},
+      { type: 'output', amount: '"Thank you for listening to my tale.  Now I may rest in peace."  He begins flickering, and waves his hands at you.'},
+      { type: 'buff', name: 'hearty', amount: {
+        amount: 2, attribute: 'spirit', duration: 10
+        }
+      },
+      { type: 'output', amount: '"Be full of heart," he whispers, and is then gone.  Well, at least you got something for wasting your time.'}
+    ],
+    fail: [
+      { type: 'output', amount: 'Turns out you\'re afraid.  You run away like the coward you are.'}
+    ]
+  },
+  {
+    title: 'Bloody Wall',
+    desc: 'A single, lone wall is bleeding.  As far as you know, walls are not supposed to bleed.  There\'s probably a reason for this.  This seems like the kind of thing you should look into.',
+    tags: ['haunted'],
+    challenge: {
+      attribute: 'mind',
+    },
+    attack_alias: ['lookatblood', 'You look at the blood oozing from the wall, trying to determine the source.'],
+    success: [
+      { type: 'output', amount: 'There appears to be a stab wound in the wall.  You bandage up the hole.'},
+      { type: 'output', amount: 'The bleeding has stopped.  With your hands covered in wall-blood, you feel good about your deed.  You have saved a wall.  You continue on with your life, knowing that while some walls bleed, at least they can be healed.'}
+    ],
+    fail: [
+      { type: 'output', amount: 'With your hands covered in wall-blood, you feel bummed out that you cannot find the source of the bleeding.  Because of this, the wall will likely die, unmourned and alone.'}
+    ]
+  },
+  {
+    title: 'Ouija Board',
+    desc: 'A ouija board drops out of the sky and lands at your feet.  What an auspicious sign!',
+    tags: ['haunted'],
+    challenge: {
+      attribute: 'mind', /* Hard difficulty, higher than normal rating for level? */
+    },
+    attack_alias: ['playwithboard', 'You decide to play with the ouija board. Maybe there is a spirit around that has something important to tell you.  What\'s the worst that could happen?'],
+    success: [
+      { type: 'output', amount: 'After asking the usual amount of pornographic questions, you find out that this ouija board is in fact the spirit of a DEAD OUIJA BOARD!'},
+      { type: 'output', amount: 'Dun Dun DUN!'},
+      { type: 'output', amount: 'It just wanted to play a game and remember it\'s life as a novelty party item.  It thanks you (by spelling out "Thanks") and offers to let you carry it around for awhile.'},
+      /*
+      { type: 'pet', name: 'Ouija Board', gives spirit bonus}
+      */
+    ],
+    fail: [
+      { type: 'output', amount: 'You are unable to get the ouija board to communicate anything useful.  Apparently novelty party games are neither useful or informative.'}
+    ]
   }
 ]
--- hacknslack.dev/game/static/encounters/ruins.js
+++ hacknslack.dev/game/static/encounters/ruins.js
@@ -12,11 +12,41 @@ module.exports = [
   }
 */
   {
-    title: 'basic ruins',
-    desc: 'ergh',
+    title: 'Old Crumbling Tower',
+    desc: 'Ahead of you is a crumbling tower.  The door is blocked by a cave-in, but there is one window high above.  If you\'ve been practicing your pull-ups like your P.E. teacher always told you, you may be able to get in.',
     tags: ['ruins'],
     challenge: {
-      attribute: 'mind'
-    }
+      attribute: 'body',
+    },
+    attack_alias: ['grabwindow', 'You jump up, reaching for the stone window sill.'],
+    success: [
+      { type: 'output', amount: 'Your exercises have finally paid off for something useful!  You are able to pull yourself up through the window.  Inside is a single chest.  You open it.'},
+      /*
+      { type: 'item', name: '%random magic item%' }
+      },
+      */
+      { type: 'output', amount: 'What a pretty bauble!  Surely this will come in handy someday.'}
+    ],
+    fail: [
+      { type: 'output', amount: 'You never practiced.  You were a failure in P.E. and you are a failure now. You\'ve never accomplished a single push-up in your life, much less a pull-up, which as everyone knows, is much harder.  Ah well, the mysteries of the crumbling tower shall mysterious.'}
+    ]
+  },
+  {
+    title: 'Rubble Everywhere',
+    desc: 'The path ahead of you has been mostly blocked by rubble.  Though it looks very dangerous, you can attempt to scale the pile of broken stone, shattered glass, and sharp pointy things.',
+    tags: ['ruins'],
+    challenge: {
+      attribute: 'body',
+    },
+    attack_alias: ['scalerubble', 'You take a deep breath and haphazardly jump onto the pile, moving up as quickly as possible.'],
+    success: [
+      { type: 'output', amount: 'You climb over the rubble effortlessly.  You could probably do some parkour if you really wanted.  But who would want to do that?'},
+      { type: 'output', amount: 'On the other side of the rubble, your path continues.  You are rewarded with the ability to continue on your journey.  Go you.'}
+    ],
+    fail: [
+      { type: 'output', amount: 'Your parkour skills are not so fancy as you imagine.  You cut your hands, shins, and face on innumerable shards of glass.  Why was there so much glass here?!?!'},
+      { type: 'dmg', amount: '3'},
+      { type: 'output', amount: 'You are able to continue on your journey, leaving a trail of blood in your wake.'}
+    ]
   }
 ]
