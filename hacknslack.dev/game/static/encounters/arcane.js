c28d3ed92dd2f55a2b5a0f942a181736301bc7f2
BDaggerhart <brandon.daggerhart@gmail.com>
2014-12-08 11:07:31 -0500

    arcane encounter

--- hacknslack.dev/game/static/encounters/arcane.js
+++ hacknslack.dev/game/static/encounters/arcane.js
@@ -12,11 +12,21 @@ module.exports = [
   }
 */
   {
-    title: 'basic arcane encounter',
-    desc: 'this thing be basic',
-    tags: ['arcane'],
+    title: "Riddling Wizards",
+    desc: "Leaning against a wall are three chatting wizards.  You can tell they are wizards because of their tall pointy hats. They call you over and ask you to solve their riddle.  If you do, they will help you out.",
+    tags: ["arcane"],
     challenge: {
-      attribute: 'mind'
-    }
-  }
+      attribute: "mind"
+    },
+    attack_alias: ['solveriddle', "Shout out the answer with great vociferousness!"],
+    success: [
+      { type: 'output', amount: 'Correct, the answer was "Biscuits!."  Way to go!'},
+      { type: 'output', amount: '"We will now make you flamey" the wizards sat in unison.  They wave their hands, and through some hocus pocus, your weapon glows with orange light.'}
+      { type: 'buff', name: 'flamey', amount: {
+        amount: 2, attribute: 'body', duration: 10
+        }
+      },
+      { type: 'output', amount: '"Your weapon is now flamey.  It will hurt bad guys more.  But only for a little while."'}
+    ]
+  } 
 ]
