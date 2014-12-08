d6390b296acbbc04b00672e0f1547b36c47ce0b2
BDaggerhart <brandon.daggerhart@gmail.com>
2014-12-08 13:05:29 -0500

    sketch for minotaur adventure

---
+++ hacknslack.dev/game/static/adventures/theLostMinotaur.js
@@ -0,0 +1,19 @@
+/*
+
+A wizard has cast a befuddling spell on a Minotaur, so that he can
+steal the Minotaur's treasures.  Now the Minotaur needs help getting back to the
+center of his own labyrinth.
+
+- Minotaur approaches, asks for aid
+- Leads to labyrinth, a huge pyramid in the middle of a field that you didn't notice before now
+- Answer riddle to enter
+- Decide on going left or right (left is correct path)
+-- If choosing correct (left) path, 8 more encounters, and 3 total treasures at end.
+-- If choosing incorrect path (right), 12 more encounters, and 1 total treasure at end.
+- # of encounters, pulled from 'Arcane' and 'Ruins'
+- At center of labyrinth, discover wizard putting treasures in his pockets.
+- Wizard encounter - if you chose left path, you may fight him and kill him, getting 3 treasures.
+- Wizard encounter - if you chose right path, he manages to disappear, taking most of the treasure with him, but you get one.
+- Minotaur thanks you and offers you coffee (boosts spirit) for next 10 encounters.
+
+*/
